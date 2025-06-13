28bf2604397ea5408e6f91d6791cb77f93af37d2
mahkamovmuzaffar <maxalphaengineer@gmail.com>
2025-06-13 06:35:19 +0500

    - Added: Unit test init

---
+++ tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from v1.models import Partner, AccessToken
+
+
+class PartnerModelTests(TestCase):
+
+    def test_create_partner(self):
+        user = Partner.objects.create_user(username="user1", password="secret")
+        self.assertTrue(user.check_password("secret"))
+        self.assertTrue(user.secret is not None)
+        self.assertTrue(user.path_to_protected_zip.exists())
+
+    def test_token_generation(self):
+        user = Partner.objects.create_user(username="user2", password="test")
+        token = AccessToken.objects.create(Partner=user)
+        token.generate()
+        self.assertTrue(token.key)
+        self.assertIn("access_token", token.rpc_result()["result"])
