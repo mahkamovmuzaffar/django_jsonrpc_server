57228c560f25fd7dded026394447eb895ffb8698
mahkamovmuzaffar <maxalphaengineer@gmail.com>
2025-08-29 21:31:28 +0500

    - Added: JSONRPC Validation for params

--- v1/views.py
+++ v1/views.py
@@ -59,6 +59,7 @@ class TransferCreateSerializer(serializers.Serializer):
     amount = serializers.IntegerField(min_value=1000)
 
 
+
 class TransferCreateSerializer(serializers.Serializer):
     card_number = serializers.CharField(min_length=16, max_length=19)
     expire = serializers.CharField(min_length=4, max_length=5)  # MMYY or MM/YY
