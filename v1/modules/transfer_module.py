96ac833565782d08d9c3962d301b40a39b466c90
mahkamovmuzaffar <maxalphaengineer@gmail.com>
2025-09-25 06:27:29 +0500

    - Added: First module transfer_module.py

--- v1/modules/transfer_module.py
+++ v1/modules/transfer_module.py
@@ -1,2 +1,15 @@
 # Created by Muzaffar Makhkamov on 6/6/23, 11:24 PM
 # Please make contact with me if you have any questions.
+def create(user, ext_id):
+    # Implement your logic here
+    pass
+
+
+def confirm(user, ext_id):
+    # Implement your logic here
+    pass
+
+
+def state(user, ext_id):
+    # Implement your logic here
+    pass
