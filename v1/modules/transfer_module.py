a42d948df004030bb13614581e8a508f6ea65c2e
mahkamovmuzaffar <maxalphaengineer@gmail.com>
2025-09-26 22:18:21 +0500

    - Added: transfer_module.py enhancement

--- v1/modules/transfer_module.py
+++ v1/modules/transfer_module.py
@@ -1,6 +1,6 @@
 # Created by Muzaffar Makhkamov on 6/6/23, 11:24 PM
 # Please make contact with me if you have any questions.
-def create(user, ext_id):
+def create(user, ext_id, sender, receiver, amount, currency):
     # Implement your logic here
     pass
 
