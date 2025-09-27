26af0d2a8e46d5d7f7a5e4bb30ed40261b9dd6be
mahkamovmuzaffar <maxalphaengineer@gmail.com>
2025-09-27 22:22:21 +0500

    - Added: ref num logic added

--- v1/modules/transfer_module.py
+++ v1/modules/transfer_module.py
@@ -1,6 +1,8 @@
 # Created by Muzaffar Makhkamov on 6/6/23, 11:24 PM
 # Please make contact with me if you have any questions.
 from v1.models.transfer import Transfer
+import uuid
+from datetime import datetime
 
 
 def create(user, ext_id, sender, receiver, amount, currency):
@@ -12,7 +14,9 @@ def create(user, ext_id, sender, receiver, amount, currency):
         return {'error': 'ext_id already exists'}
     # ...continue with creation logic...
 
-    ref_num = f"REF{user.id}{ext_id}"
+    now = datetime.now().strftime('%m%d%y%H%M%S')
+    uid = str(uuid.uuid4()).replace('-', '')[:10]
+    ref_num = f"{user.id}-{now}-{uid}"
 
     cheque = Transfer.objects.create(
         user=user,
